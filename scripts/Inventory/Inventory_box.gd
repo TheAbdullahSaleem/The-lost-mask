6762ef990c6f24bbd8c78250c2776a9d8f46352e
Abdullah Saleem <abdullah.322131@gmail.com>
2026-09-26 18:43:36 +0500

    Revert "Still configuring the inventory"
    This reverts commit 6dd69ceed6f8a0794417eb20ef06e327f86691bd.

--- scripts/Inventory/Inventory_box.gd
+++ scripts/Inventory/Inventory_box.gd
@@ -7,9 +7,18 @@ extends Panel
 func _ready() -> void:
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-func update_inventory(Name_of_item,New_quantity):
-	if self.name == Name_of_item:
-		item_count = New_quantity
+func display_item(texture: Texture2D,amount:int):
+	while item_icon == null or item_count == null:
+		print("Waiting")
+		await get_tree().process_frame
+	if amount<= 0 :
+		item_icon.texture = null
+		item_count.text = ""
+	else:
+		item_icon.texture = texture
+		item_count.text = str(amount)
+		
